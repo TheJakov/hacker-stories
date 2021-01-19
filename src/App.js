bd020b2cc3e2e51dd66f28b23029f67e500a7e5d
TheJakov <kristovic.jakov@gmail.com>
2021-01-19 22:41:10 +0100

    Little more work with lists

--- src/App.js
+++ src/App.js
@@ -43,7 +43,13 @@ function App() {
       {list.map(function(item) {
         return (
           <div key={item.objectID}>
-            {item.title}
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <br />
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
           </div>
         );
       })}
