64deba33457fd3c29e58ab1cb15f697030bc027c
TheJakov <kristovic.jakov@gmail.com>
2021-01-19 22:56:11 +0100

    Creating another React component

--- src/App.js
+++ src/App.js
@@ -40,21 +40,26 @@ function App() {
 
       <hr />
 
-      {list.map(function(item) {
-        return (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <br />
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        );
-      })}
+      <List />
+
     </div>
   );
+}
+
+function List(){
+  return list.map(function(item)  {
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <br />
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    );
+  }); 
 }
 
 export default App;
