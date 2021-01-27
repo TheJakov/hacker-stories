031c8ddc5a1e3290e5dad697121cbb1ac117b09b
TheJakov <kristovic.jakov@gmail.com>
2021-01-28 00:19:31 +0100

    Reusable React Component

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <InputWithLabel id="search" label="Search" value={searchTerm} onInputChange={handleSearch} />
 
       <hr />
 
@@ -57,11 +57,11 @@ const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
   );
 }
 
-const Search = ({ search, onSearch }) => {
+const InputWithLabel = ({ id, label, value, type='text', onInputChange }) => {
   return (
     <>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" value={search} onChange={onSearch} />
+      <label htmlFor={id}>{label}</label>
+      <input id={id} type={type} value={value} onChange={onInputChange} />
     </>
   );
 };
