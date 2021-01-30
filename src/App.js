65af45717d6266db16464eefbbeed0357fc6b649
TheJakov <kristovic.jakov@gmail.com>
2021-01-31 00:50:52 +0100

    React Asynchronous Data

--- src/App.js
+++ src/App.js
@@ -19,6 +19,14 @@ const initialStories = [
   },
 ];
 
+const getAsyncStories = () =>
+  new Promise(resolve => 
+    setTimeout(
+      () => resolve({ data: { stories: initialStories } } ),
+      2000
+    )
+  );
+
 const App = () => {
 
   
@@ -35,7 +43,13 @@ const App = () => {
     return [value, setValue];
 };
 
-const [stories, setStories] = React.useState(initialStories);
+const [stories, setStories] = React.useState([]);
+
+React.useEffect(() => {
+  getAsyncStories().then(result => {
+    setStories(result.data.stories);
+  });
+}, []);
 
 const handleRemoveStory = item => {
   const newStories = stories.filter(
