961d796bc565dd6dbc2be9460b1aaf1772e34547
TheJakov <kristovic.jakov@gmail.com>
2021-01-31 14:24:59 +0100

    React Advanced State
    Using React's useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 const initialStories = [
   {
@@ -29,8 +29,6 @@ const getAsyncStories = () =>
 
 const App = () => {
 
-  
-
   const useSemiPersistentState = (key, initialState) => {
     const [value, setValue] = React.useState(
       localStorage.getItem(key) || initialState
@@ -43,7 +41,20 @@ const App = () => {
     return [value, setValue];
 };
 
-const [stories, setStories] = React.useState([]);
+const storiesReducer = (state, action) => {
+  switch(action.type) {
+    case 'SET_STORIES':
+      return action.payload;
+    case 'REMOVE_STORY':
+      return state.filter(
+        story => action.payload.objectID !== story.objectID
+      );
+    default:
+      throw new Error();
+  }
+};
+
+const [stories, dispatchStories] = React.useReducer(storiesReducer, []);
 const [isLoading, setIsLoading] = React.useState(false);
 const [isError, setIsError] = React.useState(false);
 
@@ -51,18 +62,20 @@ React.useEffect(() => {
   setIsLoading(true);
 
   getAsyncStories().then(result => {
-    setStories(result.data.stories);
+    dispatchStories({
+      type: 'SET_STORIES',
+      payload: result.data.stories,
+    });
     setIsLoading(false);
   })
   .catch(() => setIsError(true));
 }, []);
 
 const handleRemoveStory = item => {
-  const newStories = stories.filter(
-    story => item.objectID !== story.objectID
-  );
-
-  setStories(newStories);
+  dispatchStories({
+    type: 'REMOVE_STORY',
+    payload: item,
+  });
 };
 
 const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
