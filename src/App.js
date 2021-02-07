91750aa98b8913b47c735dbf4636e47cb42a00bf
TheJakov <kristovic.jakov@gmail.com>
2021-02-07 17:14:34 +0100

    SVGs in React
    CSS rolled back to CSS modules
    
    - added SVG icon to item button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import styled from 'styled-components'
+
+import {ReactComponent as Check} from './check.svg';
+import styles from './App.module.css';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -98,8 +100,8 @@ const App = () => {
   };
 
   return (
-    <StyledContainer>
-      <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
+    <div className={styles.container}>
+      <h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
       
       <SearchForm 
         searchTerm={searchTerm}
@@ -114,7 +116,7 @@ const App = () => {
       : ( <List list={stories.data} onRemoveItem={handleRemoveStory} /> )
       }
       
-    </StyledContainer>
+    </div>
   );
 }
 
@@ -125,7 +127,7 @@ const SearchForm = (
     onSearchSubmit
   }
   ) => (
-    <StyledSearchForm onSubmit={onSearchSubmit} >
+    <form onSubmit={onSearchSubmit} className={styles.searchForm}>
         <InputWithLabel 
           id="search" 
           isFocused 
@@ -135,10 +137,10 @@ const SearchForm = (
           <strong>Search: </strong>
         </InputWithLabel>
 
-        <StyledButtonLarge type="submit" disabled={!searchTerm} >
+        <button type="submit" disabled={!searchTerm} className={`${styles.button} ${styles.buttonLarge}`} >
           Submit
-        </StyledButtonLarge>
-      </StyledSearchForm>
+        </button>
+      </form>
   );
 
 const InputWithLabel = ({ id, value, type='text', onInputChange, isFocused, children }) => {
@@ -153,9 +155,9 @@ const InputWithLabel = ({ id, value, type='text', onInputChange, isFocused, chil
 
   return (
     <>
-      <StyledLabel htmlFor={id}>{children}</StyledLabel>
+      <label htmlFor={id} className={styles.label}>{children}</label>
       &nbsp;
-      <StyledInput id={id} ref={inputRef} type={type} value={value} onChange={onInputChange} />
+      <input id={id} ref={inputRef} type={type} value={value} onChange={onInputChange} className={styles.input} />
     </>
   );
 };
@@ -168,99 +170,21 @@ const List = ({ list, onRemoveItem }) =>
 
 const Item = ({ item, onRemoveItem }) => {
   return(
-    <StyledItem>
-      <StyledColumn width="40%">
+    <div className={ styles.item }>
+      <span style={{width: '40%' }}>
         <a href={item.url}>{item.title}</a>
-      </StyledColumn>
-      <StyledColumn width="30%">{item.author}</StyledColumn>
-      <StyledColumn width="10%">{item.num_comments}</StyledColumn>
-      <StyledColumn width="10%">{item.points}</StyledColumn>
-      <StyledColumn width="10%">
-        <StyledButtonSmall type="button" onClick={() => onRemoveItem(item)}>
-          Dismiss
-        </StyledButtonSmall>
-      </StyledColumn>
-    </StyledItem>
+      </span>
+      <br />
+      <span style={{ width: '30%' }}>{item.author}</span>
+      <span style={{ width: '10%' }}>{item.num_comments}</span>
+      <span style={{ width: '10%' }}>{item.points}</span>
+      <span style={{ width: '10%' }}>
+        <button type="button" onClick={() => onRemoveItem(item)} className={`${styles.button} ${styles.buttonSmall}`}>
+          <Check height="18px" width="18px" />
+        </button>
+      </span>
+    </div>
   )
 };
 
 export default App;
-
-const StyledContainer = styled.div`
-  height: 100vw;
-  padding: 20px;
-
-  background:  #83a4d4;
-  background: linear-gradient(to left, #b6fbff, #83a4d4);
-
-  color: #171212;
-`;
-
-const StyledHeadlinePrimary = styled.h1`
-  font-size: 48px;
-  font-weight: 300;
-  letter-spacing: 2px;
-`;
-
-const StyledItem = styled.div`
-  display: flex;
-  align-items: center;
-  padding-bottom: 5px;
-`;
-
-const StyledColumn = styled.span`
-  padding: 0 5px;
-  white-space: nowarp;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-
-  a {
-    color: inherit;
-  }
-
-  width: ${props => props.width};
-`;
-
-const StyledButton = styled.button`
-  background: transparent;
-  border: 1px solid #171212;
-  padding: 5px;
-  cursor: pointer;
-
-  transition: all 0.1s ease-in;
-
-  &:hover {
-    background: #171212;
-    color: #ffffff;
-  }
-`;
-
-const StyledButtonLarge = styled(StyledButton)`
-  padding: 5px;
-`;
-
-const StyledButtonSmall = styled(StyledButton)`
-  padding: 10px;
-`;
-
-const StyledSearchForm = styled.form`
-  padding: 10px 0 20px 0;
-  display: flex;
-  align-items: baseline;
-`;
-
-const StyledLabel = styled.label`
-  border-top: 1px solid #171212;
-  border-left: 1px solid #171212;
-  padding-left: 5px;
-  font-size: 24px;
-`;
-
-const StyledInput = styled.input`
-  border: none;
-  border-bottom: 1px solid #171212;
-  background-color: transparent;
-
-  font-size: 24px;
-`;
